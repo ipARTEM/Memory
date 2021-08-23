7fadcc731c9b2f3ce148c244247e24100c09ed01
Artem <a.khimin@yandex.ru>
2021-08-23 13:37:46 +0300

    тест

--- Memory/Program.cs
+++ Memory/Program.cs
@@ -6,10 +6,84 @@ using System.Threading.Tasks;
 
 namespace Memory
 {
+
+    public class MyClass:MyClass2,In
+    {
+
+    }
+
+    public class MyClass2
+    {
+
+    }
+
+    public struct MyStract:In
+    {
+
+    }
+    public interface In
+    {
+
+    }
+
     class Program
     {
+        static void OkValue(int a)
+        {
+            a = a * 2;
+            Console.WriteLine(a);
+
+        }
+
+        static void OkIntRef(ref int a)
+        {
+            a = a * 2;
+            Console.WriteLine(a);
+        }
+
+        static void OkReference(int[] b)
+        {
+            b[0] = 10;
+
+        } 
+
+        static void OkRef(int []b)
+        {
+            b = null;
+            Console.WriteLine(b+" удалили ссылку на массив");
+        }
+
+        static void OkRef(ref int[] b)
+        {
+            b = null;
+            Console.WriteLine(b + " удалили ссылку на массив");
+        }
+
+
         static void Main(string[] args)
         {
+            int a = 2;
+
+            int[] b= new int[5];
+
+            
+            OkValue(a);                             // структура, значимый тип
+            Console.WriteLine(a);
+
+            OkIntRef(ref a);                       // передача по ссылке
+            Console.WriteLine(a);
+
+            Console.WriteLine(b[0]);
+            OkReference(b);                       
+            Console.WriteLine(b[0]);              // ссылочный тип, работа с данными в управляемой куче
+
+
+            OkRef(b);
+            Console.WriteLine(b[0]+ " - массив b");
+            OkRef(ref b);
+            Console.WriteLine(b[0] + " - массив b");
+
+
         }
     }
 }
---
+++ MemoryNET5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MemoryNET5
+{
+    class Program
+    {
+        static ref int Ok(int[] num)
+        {
+            return ref num[0];
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 7, 8, 9 };
+
+
+            int a = arr[0];
+            a = 77;
+            Console.WriteLine("Поле а: " +a+ " массив arr[0]: "+arr[0]);
+
+            ref int aa =ref arr[0];
+            aa = 77;
+            Console.WriteLine("Поле аa " + aa + " массив arr[0]: " + arr[0]);
+
+            int b =  Ok(arr);
+            b = 88;
+            Console.WriteLine("Поле аaa " + b + " массив arr[0]: " + arr[0]);
+
+            ref int bb =ref Ok(arr);
+            bb = 88;
+            Console.WriteLine("Поле аaa " + bb + " массив arr[0]: " + arr[0]);
+
+        }
+    }
+}
